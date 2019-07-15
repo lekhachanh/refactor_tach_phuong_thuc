b516ce0738d0c5a817d9e6f1dd6b633ef49c5dee
lekhachanh <lekhachanh123@gmail.com>
2019-07-15 19:45:47 +0700

    first commit

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,8 +1,20 @@
 public class Cylinder {
     public static double getVolume(int radius, int height){
-        double baseArea = Math.PI * radius * radius;
-        double perimeter = 2 * Math.PI  * radius;
-        double volume = perimeter * height + 2 * baseArea;
+        double baseArea = getBaseArea(radius);
+        double perimeter = getPerimeter(radius);
+        double volume = getVolume(height, baseArea, perimeter);
         return volume;
+    }
+
+    private static double getVolume(int height, double baseArea, double perimeter) {
+        return perimeter * height + 2 * baseArea;
+    }
+
+    private static double getPerimeter(int radius) {
+        return 2 * Math.PI  * radius;
+    }
+
+    private static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
     }
 }
---
+++ test/CylinderTestTest.java
@@ -0,0 +1,26 @@
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(Arquillian.class)
+public class CylinderTestTest {
+    @Deployment
+    public static JavaArchive createDeployment() {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(CylinderTest.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @org.junit.Test
+    public void testGetVolume0And0() {
+    }
+
+    @org.junit.Test
+    public void testGetVolume1And2() {
+    }
+}
